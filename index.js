a1476e1c065475ea4c51261562f292567aa21e7b
tylerfire55 <tylergolpe@yahoo.com>
2023-09-08 21:37:49 -0400

    Idk what is updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let gridSize = 16;
-let currentColor = "white";
+let currentColor = "#00FF00";
 const colorPicker = document.querySelector(".colorPicker");
-let userInput = document.querySelector(".gridSize");
+const userInput = document.querySelector(".gridSize");
 
 //Grid creation
 const grid = document.querySelector(".grid")
@@ -22,6 +22,7 @@ gridCreate(gridSize);
 //Color selector
 function setColor(e){
     if (e.type === "mouseover" && !mouseDown) return;
+    
     e.target.style.backgroundColor = currentColor;
 }
 
@@ -30,8 +31,8 @@ colorPicker.addEventListener("input", e=>{
 })
 
 let mouseDown = false;
-grid.addEventListener("mousedown", e => {mouseDown = true})
-grid.addEventListener("mouseup", e => {mouseDown = false})
+grid.addEventListener("mousedown", e => {mouseDown = true});
+grid.addEventListener("mouseup", e => {mouseDown = false});
 
 
 //Accept user entry for grid size
@@ -41,4 +42,20 @@ userInput.addEventListener("mouseup", e => {
     gridCreate(gridSize);
 })
 
-//
+//Rainbow effect
+
+//Shadow effect
+
+//Buttons effect each other
+const colorButton = document.querySelector(".colorButton");
+const rainbowButton = document.querySelector(".rainbowButton");
+const shadowButton = document.querySelector(".shadowButton");
+
+
+colorButton.addEventListener("click", activeButton);
+rainbowButton.addEventListener("click", activeButton);
+shadowButton.addEventListener("click", activeButton);
+
+function activeButton() {
+    
+}
